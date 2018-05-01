3df67f33426875ba8771c9aeef55c0806d24ad5b
Kelsey Campbell <campkels@gmail.com>
2018-04-30 22:54:55 -0400

    Words over time

--- analysis/words.R
+++ analysis/words.R
@@ -4,9 +4,11 @@ setwd("C:/Users/Kelsey/Desktop/Gender-Tracker/data")
 install.packages("tidytext")
 install.packages("stringr")
 install.packages("tidyverse")
+install.packages("libridate")
 library(tidytext)
 library(tidyverse)
 library(stringr)
+library(libridate)
 
 # Read in Data
 #-------------------------------------------
@@ -28,14 +30,13 @@ log$color <- ifelse((log$scale >= -5 & log$scale <= -2), "lightblue",
 
 log[,c(2,7,8)]
 
+##---------------------------------------------------------------------
+## Basic Word Counts
+##---------------------------------------------------------------------
 
-##---------------------------------------------------------------------
-## Text Analysis
-##---------------------------------------------------------------------
-                   
 # Customize Stopword List
 my_stop_words <- tibble( word = c("im"))
-not_stopwords <- tibble( word = c("high"))
+not_stopwords <- c("high", "good", "ok", "okay", "want")
 
 all_stop_words <- stop_words %>%
   bind_rows(my_stop_words) %>%
@@ -47,6 +48,15 @@ log2 <- log %>%
   mutate(text = str_to_lower(thoughts, locale = "en")) %>%
   unnest_tokens(word, text) %>%
   filter(!word %in% all_stop_words$word,str_detect(word, "[a-z]"))
+
+# Word Counts by Gender
+word_counts <- log2 %>%
+  count(word, gender) 
+
+##---------------------------------------------------------------------
+## Significant Words
+##---------------------------------------------------------------------
+# From https://www.tidytextmining.com/twitter.html#comparing-word-usage
 
 # Overall Log Odds Ratio of Words (Masc & Fem)
 word_ratios <- log2 %>%
@@ -128,7 +138,59 @@ word_ratios_post %>%
 ratios_prepost <- full_join(word_ratios_pre, word_ratios_post, by = c("word")) %>%
   mutate(diff = logratio.x - logratio.y)
 
-
 # Not sure if enough data to do the pre/post justice. 
 # Maybe with select words
-#Prob just show Overall or Pre and show post changes another way
+# Prob just show Overall or Pre and show post changes another way
+
+##---------------------------------------------------------------------
+## Significant Words Over Time
+##---------------------------------------------------------------------
+
+words_by_time_sep <- log2 %>%
+  mutate(time_floor = floor_date(date2, unit = "3 month")) %>%
+  count(time_floor, gender, word) %>%
+  ungroup() %>%
+  group_by(gender, time_floor) %>%
+  mutate(time_total = sum(n)) %>%
+  group_by(word) %>%
+  mutate(word_total = sum(n)) %>%
+  ungroup() %>%
+  rename(count = n)
+
+words_by_time_sep %>%
+  filter(word %in% c( "frustrated", "good")) %>%
+  filter(gender == "Masculine") %>%
+  ggplot(aes(time_floor, count/time_total, color = word)) +
+  geom_line(size = 1.3) +
+  labs(x = NULL, y = "Word frequency")
+
+words_by_time_sep %>%
+  filter(word %in% c("confident", "comfortable", "happy")) %>%
+  filter(gender == "Feminine") %>%
+  ggplot(aes(time_floor, count/time_total, color = word)) +
+  geom_line(size = 1.3) +
+  labs(x = NULL, y = "Word frequency")
+
+#Overall
+
+words_by_time <- log2 %>%
+  mutate(time_floor = floor_date(date2, unit = "3 month")) %>%
+  count(time_floor, word) %>%
+  ungroup() %>%
+  group_by(time_floor) %>%
+  mutate(time_total = sum(n)) %>%
+  group_by(word) %>%
+  mutate(word_total = sum(n)) %>%
+  ungroup() %>%
+  rename(count = n)
+
+words_by_time
+
+words_by_time %>%
+  filter(word %in% c("control")) %>%
+  ggplot(aes(time_floor, count/time_total, color = word)) +
+  geom_line(size = 1.3) +
+  labs(x = NULL, y = "Word frequency")
+
+# Some interesting words, but i suspect most changes are due to just shifts in my language...
+# Check if need to trim date ends
